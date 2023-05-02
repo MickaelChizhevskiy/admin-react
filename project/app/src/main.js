f47c40ba2e33940221b497341b669cb9d0b04e90
Michael Chizhevskiy <michael.chizhevskiy@nimbusweb.me>
2023-05-02 23:28:14 +0300

    deleted jquery

--- project/app/src/main.js
+++ project/app/src/main.js
@@ -4,26 +4,3 @@ import { createRoot } from "react-dom/client";
 
 
 createRoot(document.getElementById("root")).render(<Editor/>);
-// function getPageList(){
-//     $("h1").remove();
-
-//     $.get("./api", data => {
-//         data.forEach(file => {
-//             $("body").append(`<h1>${file}</h1>`)
-//         })
-//     }, "JSON");
-
-// }
-
-// getPageList();
-
-// $("button").click(() => {
-//     $.post("./api/createNewPage.php", {
-//         "name" : $("input").val()
-//     }, () => {
-//         getPageList();
-//     })
-//     .fail(() => {
-//         alert("Page allready exist!");
-//     })
-// });
